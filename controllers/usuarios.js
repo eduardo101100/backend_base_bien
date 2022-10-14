3987621e558083b981424517bb35f896a519292c
Eduardo Lopez Urbano <isc18350724@gmail.com>
2022-10-14 10:56:54 -0500

    conexion a la base de datos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,7 +1,28 @@
 const { request, response } = require("express");
-
-const getUsers = (req = request, res = response) =>{
-    console.log("funcion getUsers")
-    res.json({msg: "Funcion getUsers"})
+const pool = require("../db/connection")
+const getUsers = async (req = request, res = response) =>{
+    //estructura basica de cualquier endpoint al conectar en su BD
+    
+    let conn;
+    //control de exepciones
+    try {
+        conn = await pool.getConnection()
+        //esta es la consulta mas basica, se pueden hacer mas complejas
+        const users = await conn.query("SELECT * FROM Usuarios", (error) => {throw new Error(error) })
+        //siempre validar que no se obtuvieron resultados
+        if (!users) {
+            res.status(404).json({msg:"no se encontraron registros"})
+            return
+        }
+        res.json({users})
+        //lo del cath y final siempre sera lo mismo
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }finally{
+        if(conn){
+            conn.end()
+        }
+    }
 }
 module.exports = {getUsers}
